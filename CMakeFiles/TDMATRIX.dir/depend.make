89dac36da1a3f1f1557dab4ec59745f2ed5cd52c
Jane <mynameszhenya@yandex.ru>
2025-02-08 15:48:37 +0300

    Added smt

---
+++ CMakeFiles/TDMATRIX.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TDMATRIX.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/TDMATRIX.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TDMATRIX.dir/src/test.cpp.o"
+  "CMakeFiles/TDMATRIX.dir/src/test.cpp.o.d"
+  "TDMATRIX"
+  "TDMATRIX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TDMATRIX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
